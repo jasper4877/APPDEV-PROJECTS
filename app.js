540882b72ca046fea398a9ea3311161f7265accb
jasper4877 <57189804+jasper4877@users.noreply.github.com>
2024-08-21 16:39:54 +0800

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const navToggle = document.querySelector('nav-toggle')
-const links = document.querySelector('.links')
-navToggle.addEventListener('click', () => {
-    links.classList.toggle('show-links')
-})
